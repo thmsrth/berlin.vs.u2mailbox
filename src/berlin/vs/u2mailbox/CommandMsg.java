7e16a4146da64376e99f544fc89ba3950b1cd66e
thmsrth <thoemu.roth@gmail.com>
2015-05-18 19:17:10 +0200

    msg angepasst

--- src/berlin/vs/u2mailbox/CommandMsg.java
+++ src/berlin/vs/u2mailbox/CommandMsg.java
@@ -18,7 +18,7 @@ public class CommandMsg extends CommandHandler {
     @Override
     public void execute() {
         ArrayList<String> sendMSG = new ArrayList<>();
-        sendMSG.add(msg.params.get(0));
+        sendMSG.add(actClient.username +": "+msg.params.get(0));
         ArrayList<String> responseMSG = new ArrayList<>();
         ClientMainFrame c = null;
         String clientS = msg.params.get(1);
@@ -31,10 +31,10 @@ public class CommandMsg extends CommandHandler {
 
         if (c != null) {
             this.sendResponse(this.createResponse(200, sendMSG), c);
-            responseMSG.add("Message: " + sendMSG.get(0) + " send successful to: " + c.username);
+            responseMSG.add("Message: " + msg.params.get(0) + " send successful to: " + c.username);
             sendResponse(this.createResponse(200, responseMSG));
         } else {
-            responseMSG.add("Message: " + sendMSG.get(0) + " can't be send to: " + clientS + ". Client not exist.");
+            responseMSG.add("Message: " + msg.params.get(0) + " can't be send to: " + clientS + ". Client not exist.");
             sendResponse(this.createResponse(404,responseMSG));
         }
     }
